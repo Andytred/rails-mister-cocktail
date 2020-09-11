00f1bede3734c2359724d4b769a7fb1835bc5780
Andrea Tredrea <andrea.tredrea@gmail.com>
2020-09-11 16:05:19 +0200

    Andytredcocktails

--- Gemfile
+++ Gemfile
@@ -22,9 +22,10 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'autoprefixer-rails'
-gem 'font-awesome-sass', '~> 5.12.0'
-gem 'simple_form'
 
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'font-awesome-sass', '~> 5.6.1'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :new, :create, :show] do 
+  root to: "cocktails#index"
+  resources :cocktails, only: [:new, :create, :show] do 
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy] 
